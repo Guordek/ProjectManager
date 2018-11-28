2094badb65002be93fa20e793baed76f17641bcf
Guordek <30309206+Guordek@users.noreply.github.com>
2018-11-28 12:56:03 +0100

    Fix

--- database/migrations/2018_11_16_202042_create_tasks_table.php
+++ database/migrations/2018_11_16_202042_create_tasks_table.php
@@ -17,8 +17,8 @@ class CreateTasksTable extends Migration
           $table->increments('id');
           $table->string('name');
           $table->longText('description');
-          $table->dateTime('start');
-          $table->dateTime('end');
+          $table->date('start');
+          $table->date('end');
           $table->integer('project_id')->unsigned();
           $table->integer('user_id')->unsigned();
           $table->integer('level_id')->unsigned();
