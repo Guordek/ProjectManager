f95c25b0a9f801467473e2f3d4393fd39de1e8ef
Guordek <30309206+Guordek@users.noreply.github.com>
2018-12-03 12:58:03 +0100

    Display error message from validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,6 +97,15 @@
               <div class="container">
                   <div class="row justify-content-center">
                     <div class="col-md-12">
+                      @if ($errors->any())
+                        <div class="alert alert-danger">
+                          <ul>
+                            @foreach ($errors->all() as $error)
+                              <li>{{ $error }}</li>
+                            @endforeach
+                          </ul>
+                        </div>
+                      @endif
                       @include('flash::message')
                     </div>
                   </div>
